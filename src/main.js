89d5794573d5d934f8e879d27e46280cfbfd4142
Stephanie Sohns <ssohns@gmail.com>
2020-03-28 18:54:36 -0600

    Input functionality to show on page

--- src/main.js
+++ src/main.js
@@ -10,12 +10,17 @@ var showSaved = document.querySelector(".show-saved");  //this is the button tha
 var showRandom = document.querySelector(".show-random"); //this button changes the poster on the main page -can cycle thru diff posters
 var showForm = document.querySelector(".show-form"); // this button takes user to the Form Page -to make their own poster
 // Second SECTION of html
-var posterForm = document.querySelector('.poster-form');
-var makePoster = document.querySelector('.make-poster'); // this is the Show My Poster button on the Form page (shows your poster on main page)
-var showMain = document.querySelector('.show-main'); // this button is also on Form page "NEVERMIND"button returns to main page
+var posterForm = document.querySelector(".poster-form");
+var makePoster = document.querySelector(".make-poster"); // this is the Show My Poster button on the Form page (shows your poster on main page)
+var showMain = document.querySelector(".show-main"); // this button is also on Form page "NEVERMIND"button returns to main page
 // Third SECTION of html
-var savedPostersPage = document.querySelector('.saved-posters'); //this is the page of many saved posters
-var backToMain = document.querySelector('.back-to-main'); //this button is on Saved Posters Page & returns user to main page
+var savedPostersPage = document.querySelector(".saved-posters"); //this is the page of many saved posters
+var backToMain = document.querySelector(".back-to-main");//this button is on Saved Posters Page & returns user to main page
+
+var imageUrlInput = document.getElementById('poster-image-url');
+var titleInput = document.getElementById('poster-title');
+var quoteInput = document.getElementById('poster-quote');
+
 // we've provided you with some data to work with 👇
 var images = [
   "./assets/bees.jpg",
@@ -114,6 +119,7 @@ var quotes = [
   "Each person must live their life as a model for others.",
   "A champion is defined not by their wins but by how they can recover when they fall."
 ];
+var savedPostersArray = [];
 
 posterImage.src = images[getRandomIndex(images)];
 posterTitle.innerText = titles[getRandomIndex(titles)];
@@ -121,10 +127,12 @@ posterQuote.innerText = quotes[getRandomIndex(quotes)];
 
 // event listeners go here 👇
 
+// showRandom.addEventListener('click', getRandomIndex);
 showForm.addEventListener('click', goToFormPage);
 showMain.addEventListener('click', returnToMainPage);
 showSaved.addEventListener('click', goToSavedPosters);
 backToMain.addEventListener('click', returnToMainPage);
+makePoster.addEventListener('click', gatherUserData);
 
 // functions and event handlers go here 👇
 
@@ -149,3 +157,51 @@ function goToFormPage() {
    mainPoster.classList.add('hidden');
    savedPostersPage.classList.remove('hidden');
  }
+
+ function gatherUserData() {
+   var imageUrl = imageUrlInput.value;
+   var title = titleInput.value;
+   var quote = quoteInput.value;
+   images.push(imageUrl);
+   titles.push(title);
+   quotes.push(quote);
+   var anotherNewPoster = new Poster(imageUrl, title, quote);
+   savedPostersArray.push(anotherNewPoster);
+   displayCreatedPoster();
+ }
+
+ function displayCreatedPoster() {
+   var imageUrl = imageUrlInput.value;
+   var title = titleInput.value;
+   var quote = quoteInput.value;
+   posterImage.src = imageUrl;
+   posterTitle.innerText = title;
+   posterQuote.innerText = quote;
+   returnToMainPage();
+ }
+
+ // function saveCreatedPosterValue() {
+ //   var displayImageUrl = posterImage.value;
+ //   var displayTitle = posterTitle.value;
+ //   var displayQuote = posterQuote.value;
+ //   posterImage.value = posterTitle.value = posterQuote.value = "";
+ //   displayCreatedPoster(displayImageUrl, displayTitle, displayQuote);
+ //   // console.log(displayImageUrl, displayTitle, displayQuote);
+ // }
+
+ // function getImageUrl() {
+ //   var imageUrl = imageUrlInput.value;
+ // }
+ //
+ // function getUserTitle() {
+ //   var title = titleInput.value;
+ // }
+ //
+ // function getUserQuote() {
+ //   var quote = quoteInput.value;
+ // }
+ // function showMyPosterBtn() {
+ //   var newUserInput = [];
+ //   for (var i = 0; i < newUserInput.length; i++);
+ //   newUserInput.push(document.querySelector('#poster-image-url', '#poster-title', '#poster-quote').value);
+ // }
